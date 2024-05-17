6e4c0b5ef7faf832a32717564462ed3e6134128a
takumi3110 <takumi3110@users.noreply.github.com>
2024-05-17 03:48:07 +0000

    deploy: d241223e649834f11b1b2ddbb0e0d06e44d17cd4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2243523495"
+    serviceWorkerVersion: "3372286790"
   }
 });
